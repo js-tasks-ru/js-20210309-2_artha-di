b6a3ef5d1da43c1cd18b0477c851ec3a6b4893d2
Oleg Teplitsky <artha_di@mail.ru>
2021-03-20 00:21:09 +0300

    task-1-create-getter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,21 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const arrayKey = path.trim().split('.');
 
+  function getValueObject (obj) {
+
+    let tempValue = obj;
+
+    if (Object.keys(obj).length === 0) {
+      return;
+    }
+
+    for (let valueKey of arrayKey) {
+      tempValue = tempValue[valueKey];
+    }
+
+    return tempValue;
+  }
+  return getValueObject;
 }
