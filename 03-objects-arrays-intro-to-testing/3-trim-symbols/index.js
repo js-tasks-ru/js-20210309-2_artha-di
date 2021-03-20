069ebed2e6a86794adc9138654e74f5f882eac76
Oleg Teplitsky <artha_di@mail.ru>
2021-03-20 23:58:43 +0300

    task-trim-symbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -4,6 +4,26 @@
  * @param {number} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-export function trimSymbols(string, size) {
 
+export function trimSymbols(string = '', size) {
+  if (!string) { return '';}
+  switch (size ?? '') {
+    case 0:
+      return '';
+      break;
+    case '':
+      return string;
+  }
+  let countChar = 1;
+  return [...string].reduce((accumulator, currentValue, i, arr) => {
+    if (countChar < size && arr[i - 1] === currentValue) {
+      countChar++;
+      return accumulator += currentValue;
+    }
+    if (arr[i - 1] !== currentValue) {
+      countChar = 1;
+      return accumulator += currentValue;
+    }
+    return accumulator;
+  });
 }
